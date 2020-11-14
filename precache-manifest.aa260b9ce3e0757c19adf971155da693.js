9b9729485b030a1ad3618f620263d215525d7055
asa121asa <crafta.net@gmail.com>
2020-11-14 19:30:32 +0300

    Updates

---
+++ precache-manifest.aa260b9ce3e0757c19adf971155da693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3135c885840a6facce30ad308d971f6b",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "fdc2224b43532371d4b6",
+    "url": "/Todolist/static/css/main.c783b697.chunk.css"
+  },
+  {
+    "revision": "731265d97c0054bf0bf0",
+    "url": "/Todolist/static/js/2.1d96c56f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todolist/static/js/2.1d96c56f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc2224b43532371d4b6",
+    "url": "/Todolist/static/js/main.38789d0e.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
